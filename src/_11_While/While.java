3ffbd4a0996265831767733466d3bf30efa5eb14
남영석 <st0nn@naver.com>
2025-10-17 16:50:09 +0900

    Feat: While문

---
+++ src/_11_While/While.java
@@ -0,0 +1,60 @@
+package _11_While;
+
+import java.util.Scanner;
+
+public class While
+{
+    static void main(String[] args) {
+//
+//        int i = 1;
+//        while ( i <= 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int i = 0;
+//        while (i<= 10){
+//            if (i%2==0) {
+//                System.out.println(i);
+//            }
+//            i++;
+//        }
+//
+//        boolean flag = true;
+        Scanner scanner = new Scanner(System.in);
+//        while (flag) {
+//            int score = scanner.nextInt();
+//            if (0 <= score && score <= 100) {
+//                System.out.println("점수는"+score);
+//                flag = false;
+//            } else {
+//                System.out.println("다시 입력하세요");
+//            }
+//
+//        }
+//
+//        int menu;
+//        While (true) {
+//            System.out.println("메뉴를 선택하세요.");
+//        }
+
+        int num = 0;
+        int count = 0;
+        while (count<=100){
+            num =+ count;
+            count++;
+        }
+        System.out.println(num);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
